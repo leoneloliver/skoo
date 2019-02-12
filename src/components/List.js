ecd224eb2e628254f5bb5afdb056f3ee773d83e4
Leonel Oliveira <leoneloliviera@Leonels-MacBook-Air.local>
2019-02-12 10:55:21 -0500

    updates

--- src/components/List.js
+++ src/components/List.js
@@ -5,36 +5,6 @@ import axios from 'axios'
 
 class List extends Component {
 
-  // constructor(props){
-  //   super(props)
-  //   this.state = {
-  //     users: [],
-  //     store: []
-  //   }
-  // }
-
-  // componentDidMount(){
-  //   // axios.get('https://randomuser.me/api/?results=50')
-  //   axios.get('https://randomuser.me/api/?results=50')
-  //   .then(json => json.data.results.map(result => (
-  //     {
-  //       name: `${result.name.first} ${result.name.last}`,       
-  //       avatar: `${result.picture.large}`,
-  //       email: `${result.email}`, 
-  //       state: `${result.location.city} - ${result.location.state}`,
-  //       phone: `${result.phone}`
-  //     })))
-  //   .then(newData => this.setState({users: newData, store: newData}))
-  //   .catch(error => alert(error))
-  // }
-
-  // filterNames(e){
-  //   this.setState({users: this.state.store.filter((item) => item.name.toLowerCase().includes(e.target.value.toLowerCase()))})
-  // }
-
-
-// --------
-
 handleClick = () => {
     function clickable(){
       var classname = document.getElementsByClassName("class-static");
@@ -43,25 +13,19 @@ handleClick = () => {
         var address = this.getAttribute("data-address");
         var email = this.getAttribute("data-email");
         var avatar = this.getAttribute("data-avatar");
-
         document.getElementById("my-modal").classList.add("show-it");
-
         alert(fullname+' - '+address+' - '+email+' - '+avatar);
-
       };
       Array.from(classname).forEach(function(element) {
         element.addEventListener("click", myFunction);
       });
-
       document.getElementById("close").addEventListener("click", closeModal);
       function closeModal(){
         document.getElementById("my-modal").classList.remove("show-it");
       }
-
     }
     setTimeout(function(){ clickable(); }, 900);
   }
-
   constructor(props) {
     super(props);
     this.handleLoad = this.handleLoad.bind(this);
@@ -70,15 +34,10 @@ handleClick = () => {
       store: []
     }
   }
-
-
   componentDidMount() {
-    
     //call function when load page list
     this.handleClick();
-  
     window.addEventListener('load', this.handleLoad);
-    // axios.get('https://randomuser.me/api/?results=50')
     axios.get('https://randomuser.me/api/?results=50')
     .then(json => json.data.results.map(result => (
       {
@@ -91,24 +50,13 @@ handleClick = () => {
     .then(newData => this.setState({users: newData, store: newData}))
     .catch(error => alert(error))
   }
-
   filterNames(e){
     this.setState({users: this.state.store.filter((item) => item.name.toLowerCase().includes(e.target.value.toLowerCase()))});
-    //call function when search 
     this.handleClick();
   }
-
   handleLoad() {
     this.handleClick();
   }
-
-
-// ---------
-
-
-
-
-
   render() {
     const {users} = this.state
     return (
@@ -117,9 +65,7 @@ handleClick = () => {
           <div className={styles['title-container']}>
             <h1 className={styles['center-mobile']}>Tutors</h1>
           </div>
-        
-        </div>
-      
+        </div>   
         <SearchBar searchFunc={(e) => this.filterNames(e)}/>
         <div id="productPage" className={styles['list-view']}>
           <ul className={styles['productWrapper']}>
@@ -146,16 +92,4 @@ handleClick = () => {
     );
   }
 }
-
 export default List;
-
-
-
-      
-
-
-
-
-
-
-          
